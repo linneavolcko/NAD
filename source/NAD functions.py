5d74de82be0b9c1b1b6835e83845a5a1e0abfd3f
Jaime McCutcheon <mccutcheon.james@gmail.com>
2022-06-17 14:50:16 +0200

    added code for testing fx

--- source/NAD functions.py
+++ source/NAD functions.py
@@ -8,7 +8,7 @@ import pickle
 # %%
 # Modified from your function. Tidied up a bit and added option for bins and variable pre-event and snip lengths.
 
-def get_snips_baseline(event, fs, signal, pre=10, length=20, bins="none"):
+def get_snips_baseline(event, fs, signal, pre=10, length=20, bins=None):
     """
     Gets snips around an event.
 
@@ -27,11 +27,38 @@ def get_snips_baseline(event, fs, signal, pre=10, length=20, bins="none"):
     
     snip_baseline_corrected = (snip - mean) / std
 
-    if bins != "none":
+    if bins != None:
         rem = len(snip_baseline_corrected) % bins
         if  rem != 0:
             snip_baseline_corrected = snip_baseline_corrected[:-rem]
         snip_reshaped = np.reshape(snip_baseline_corrected, (bins, -1))
-        snip_baseline_corrected = np.mean(snip_reshaped)
+        snip_baseline_corrected = np.mean(snip_reshaped, axis=1)
 
     return snip_baseline_corrected
+
+# # %%
+# folder = "D:\\Test Data\\photometry\\NAc GRAB photometry\\"
+
+# data = tdt.read_block(folder+"Test-220609-101142")
+
+# # %%
+# licks = data.epocs._7RL_.onset
+# sipper = data.epocs._7sp_.onset
+# blue = data.streams._4657.data
+# uv = data.streams._4057.data
+
+# fs = data.streams._4657.fs
+
+# corrected_signal = tp.processdata(blue, uv)
+# # %%
+# f, ax = plt.subplots(nrows=2, figsize=(4, 5))
+
+# snips = []
+
+# for sip in sipper:
+#     snip =get_snips_baseline(sip, fs, corrected_signal, pre=5, length=20, bins=200)
+#     snips.append(snip)
+#     ax[0].plot(snip, color="grey", alpha = 0.3)
+# # %%
+# len(snips)
+# # %%
